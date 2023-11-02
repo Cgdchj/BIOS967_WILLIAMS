34d1fca8cb127ad2623629fd4262bd8f9f503c58
dshizuka <dshizuka2@unl.edu>
2023-11-02 13:44:25 -0500

    Update Logger_2.R

--- Logger_2.R
+++ Logger_2.R
@@ -1,6 +1,15 @@
+library(tidyverse)
+library(lubridate)
 library(readr)
-Logger_2 <- read_csv("Logger 2.csv", col_types = cols(`Date/Time` = col_datetime(format = "%m/%d/%Y %H:%M")))
-View(Logger_2)
-ggplot(data=Logger_2,mapping=aes(x=`Date/Time`, y=Value))+
+#import data
+Logger_2 <- read.csv("Logger 2.csv")
+#convert "Date.Time" to date-time object and save.
+Logger_2 = Logger_2 %>% mutate(date_time=mdy_hm(Date.Time))
+
+ggplot(data=Logger_2,mapping=aes(x=date_time, y=Value))+
+  geom_path()
+
+#zoom in to specific date/time range by filtering the data
+ggplot(data=Logger_2 %>% filter(date_time>mdy_hm("6/13/2023 10:00")&date_time<mdy_hm("6/13/2023 18:00")),mapping=aes(x=date_time, y=Value))+
+  geom_path() +
   geom_point()
-str(Logger_2)
